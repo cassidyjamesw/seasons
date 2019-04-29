17d4ccc714ceaf722ac2b55d05d2c1f5d9f509de
James Cassidy <cassidyjamesw@gmail.com>
2019-04-29 10:40:01 -0400

    styling

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -16,7 +16,7 @@ const getSeason = (lat, month) => {
   if (month > 2 && month < 9) {
     return lat > 0 ? "summer" : "winter";
   } else {
-    return lat > 0 ? "winter" : "summer";
+    return lat < 0 ? "winter" : "summer";
   }
 };
 
